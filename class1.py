355ab438066d38072d05b0625ae6699ae85b0ac8
augustinejang <sjjan28@gmail.com>
2023-12-12 11:27:47 +0900

    클래스 연습 1

---
+++ class1.py
@@ -0,0 +1,21 @@
+#1)클래스 형식을 정의
+class Person:
+    #초기화 루틴을 실행
+    def __init__(self):
+        #인스턴스 멤버 변수
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+
+#2)인스턴스(복사본)을 생성(Break Point)
+p1 = Person()
+p2 = Person()
+#3)메서드 호출
+# p1.print()
+# p2.print()
+#언타임시에 변ㄱ수 추가
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
